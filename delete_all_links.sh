9ecc072e9edf7b385c5c05f860da1f669990c93a
phil <ph-ketteni@t-online.de>
2020-11-12 17:52:00 +0100

    Removed inidvidual sxhkd files, because they are equal. Updated some configs.

---
+++ delete_all_links.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm ~/.config/alacritty/alacritty.yml
+rm ~/.config/fish/config.fish
+rm ~/.config/zshrc
+rm ~/.config/kitty/kitty.conf
+rm ~/.config/starship.toml
+rm ~/.config/termite/termite_config
+rm ~/.config/bspwm/bspwmrc*
+rm ~/.config/sxhkd/sxhkdrc/sxhkdrc
+rm ~/.config/polybar/config*
--- link_all.sh
+++ link_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-cd $dir
+cd "$dir"
 
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/fish
@@ -10,13 +10,13 @@ mkdir -p ~/.config/bspwm
 mkdir -p ~/.config/sxhkd
 mkdir -p ~/.config/polybar
 
-ln -sr alacritty/alacritty.yml ~/.config/alacritty
-ln -sr fish/config.fish ~/.config/fish
-ln -sr zsh/zshrc ~/.config
-ln -sr kitty/kitty.conf ~/.config/kitty
+ln -sr alacritty/alacritty.yml ~/.config/alacritty/
+ln -sr fish/config.fish ~/.config/fish/
+ln -sr zsh/zshrc ~/.config/
+ln -sr kitty/kitty.conf ~/.config/kitty/
 ln -sr scripts ~
 ln -sr starship/starship.toml ~/.config/
-ln -sr termite/termite_config ~/.config/termite
-ln -sr bspwm/* ~/.config/bspwm
-ln -sr sxhkd/* ~/.config/sxhkd
-ln -sr polybar/* ~/.config/polybar
+ln -sr termite/termite_config ~/.config/termite/
+ln -sr bspwm/* ~/.config/bspwm/
+ln -sr sxhkd/sxhkdrc ~/.config/sxhkd/
+ln -sr polybar/* ~/.config/polybar/
