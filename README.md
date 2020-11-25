6f58d2ed1a84b8d8bcdce6974e03e2e7be5e842e
phil <ph-ketteni@t-online.de>
2020-11-25 19:59:59 +0100

    Update configs.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # My config Files
-Just my configs.
+Just my configs and some scripts needed by them.
 ## Zsh
 Slightly edited clone of Luke Smiths [.zshrc](https://gist.github.com/LukeSmithxyz/e62f26e55ea8b0ed41a65912fbebbe52).
-
-## Termite
--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 fish_vi_key_bindings
 set fish_greeting ""
 abbr -a -g nv nvim
+abbr -a -g sps "sudo pacman -S"
 set -g -x shell fish
 
 if set -q SSH_CONNECTION
@@ -8,5 +9,6 @@ if set -q SSH_CONNECTION
 else
   set -g -x EDITOR nvim
 end
+set PATH $PATH $HOME/.cargo/bin
 
 starship init fish | source
